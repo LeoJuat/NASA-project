54ec48374512c4056b3d61c1b38b782ff8a2e31f
leojuat <leonellejuat@gmail.com>
2023-03-19 15:29:04 -0700

    connection to MongoDB done

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,14 @@
 const http = require("http");
-const mongoose = require("mongoose");
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:Blackops1991@cluster0.wttshot.mongodb.net/nasa?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startSetver() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
 
   await loadPlanetsData();
 
